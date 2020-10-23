5c818b7a1ee117415307b570e188cfaaedae3c75
Camilo <camiloe.perafan@gmailcom>
2020-10-23 07:14:13 -0500

    add deatures with params

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 from pytest import fixture
+import json
+import os
 from config import Config
+
+data_path = os.getcwd()+'/tests/data-driven/test_data.json'
 
 
 @fixture(scope='function')
@@ -25,3 +29,25 @@ def env(request):
 def app_config(env):
     cfg = Config(env)
     return cfg
+
+
+def load_test_data(path):
+    with open(path) as data_file:
+        data = json.load(data_file)
+        return data
+
+# the parameters are in string beacause i dont wanted to isnstall webdriver
+
+
+@fixture(params=['webdriver.Chrome', 'webdriver.Firefox', 'webdriver.Edge'])
+def browser(request):
+    driver = request.param
+    drvr = driver()
+    yield drvr
+    drvr.quit()
+
+
+@fixture(params=load_test_data(data_path))
+def tv_brand_from_fixture(request):
+    data = request.param
+    return data
